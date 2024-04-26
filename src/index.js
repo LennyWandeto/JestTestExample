90b586f730fb4800e340abd64404a17003905305
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-04-26 11:15:24 +0300

    pass test

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const ele = <h1>Woah!</h1>
+ReactDOM.render(
+    ele
+
+, document.getElementById("root")
+
+);
